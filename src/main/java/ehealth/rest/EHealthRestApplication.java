939f0a0af22505aa19e3636f4ed13422d18ae1d3
max <justno@no.com>
2015-12-02 22:59:12 +0100

    initial

---
+++ src/main/java/ehealth/rest/EHealthRestApplication.java
@@ -0,0 +1,12 @@
+package ehealth.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EHealthRestApplication.class, args);
+    }
+}
